1ec80eaec4d1c4f922bf5d7cc393050f6710f214
sourav santra <souravsantra59580@gmail.com>
2020-12-17 23:54:41 +0530

    first commit

---
+++ pages/shirts/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function index() {
+    return (
+        <div>
+            <img src="/images/500_d31b0b14-cf17-4c44-8501-9f640df27ac5_grande.png" width="300px"/>
+        </div>
+    )
+}
